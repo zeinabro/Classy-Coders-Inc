8dda9835f21480d5f2fadef857b9c3ff1ac5d445
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-08 16:16:37 +0100

    sales person class

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,7 +1,20 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
+class SalesPerson extends Employees{
+    #totalSales
+    constructor(name,position,salary,clients){
+        super(name,position,salary)
+        this.clients=clients;
+        this.#totalSales=0
+    }
 
+    getSalesNumber(){
+        return this.#totalSales
+    }
 
+    makeSale(amount){
+        this.#totalSales+=amount
+    }
     
 }
 
