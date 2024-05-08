119066c5cc23d9a530f55df85a0b94766f901a57
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-08 16:11:32 +0100

    manager class

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,7 +1,28 @@
-class Manager {
+const { Employees } = require("./Employees");
 
+class Manager extends Employees{
+    #employeesManaged
+    constructor(name,position,salary,department){
+        super(name,position,salary)
+        this.department=department
+        this.#employeesManaged=[]
+    }
 
+    getDepartment(){
+        return this.department
+    }
 
+    setDepartment(department){
+        this.department=department
+    }
+    
+    getEmployeesManaged(){
+        return this.#employeesManaged
+    }
+
+    setEmployeesManaged(employee){
+        this.#employeesManaged.push(employee)
+    }
     
 }
 
