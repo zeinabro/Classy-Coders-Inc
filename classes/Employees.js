9409324344b34370e215f214a6ca20d3b454ffbd
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-08 16:07:39 +0100

    employee class without static

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,7 +1,32 @@
 class Employees {
+    #salary
+    #isHired
+    constructor(name,position,salary){
+        this.name=name
+        this.position=position
+        this.#salary=salary
+        this.#isHired=true
+    }
 
+    getSalary(){
+        return this.#salary
+    }
 
+    setSalary(amount){
+        this.#salary=amount
+    }
 
+    getStatus(){
+        return this.#isHired
+    }
+
+    setStatus(command){
+        if (command=="hire"){
+            this.#isHired=true
+        } else if (command="fire"){
+            this.#isHired=false
+        }
+    }
     
 }
 
