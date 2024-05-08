059cbd2c4c0c9857d340f1b4296ac3c3074cdfff
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-08 16:19:14 +0100

    updated employee class with static values

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,11 +1,23 @@
 class Employees {
     #salary
     #isHired
+    static allEmployees = []
     constructor(name,position,salary){
         this.name=name
         this.position=position
         this.#salary=salary
         this.#isHired=true
+        Employees.allEmployees.push(this)
+    }
+    
+    static getEmployees(){
+        return Employees.allEmployees
+    }
+
+    static getTotalPayroll(){
+        let total=0;
+        Employees.allEmployees.forEach(employee=>total+=employee.getSalary())
+        return total
     }
 
     getSalary(){
