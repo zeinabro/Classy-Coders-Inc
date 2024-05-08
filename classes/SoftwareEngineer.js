74b4fd8045108064f58dac0bacbcc796b8544506
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-08 16:13:35 +0100

    software engineer class

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,7 +1,19 @@
-class SoftwareEngineer {
+const { Employees } = require("./Employees");
 
+class SoftwareEngineer extends Employees{
+    #programmingLanguages
+    constructor(name,position,salary,programmingLanguages){
+        super(name,position,salary)
+        this.#programmingLanguages=programmingLanguages
+    }
 
+    getProgrammingLanguages(){
+        return this.#programmingLanguages
+    }
 
+    setProgrammingLanguages(language){
+        this.#programmingLanguages.push(language)
+    }
     
 }
 
